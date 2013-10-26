3cad1a089d53cc37cd738f889012a06d6e4e1425
Jon Crump <jjcrump@uw.edu>
2013-10-25 19:18:16 -0700

    All major functionality now present. Much clean-up still to do.

--- awesomeapp.py
+++ awesomeapp.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+### copyright, Opscode Inc.
+# Apache License
+# Version 2.0, January 2004
+# http://www.apache.org/licenses/
+##################################
+
 import sys
 from functools import wraps
-from hashlib import md5
+from hashlib import sha1
 from pprint import pprint
-from flask import Flask, request, flash, Response, session, g, redirect, url_for, abort, render_template
+from flask import Flask, request, flash, Response, session, g, jsonify, redirect, url_for, abort, render_template
 import MySQLdb
-from AAR_config import AUTH_USERS, SECRET_KEY, CONNECTION_ARGS
+from AAR_config import SECRET_KEY, CONNECTION_ARGS
+########### local and temporary debugging crap: ###########
+import logging
+logger = logging.getLogger('aarapp')
+hdlr = logging.FileHandler('/Users/jjc/Desktop/aarapp.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+logger.setLevel(logging.INFO)
+##########################################################
 
-silly_text = '''I'm terribly sorry. I've looked everywhere but I can't find that combination of credentials in my records. Perhaps you mistyped your password? Or maybe you simply don't have an account with us yet. I am sorry to be so thick. I'm a bad server, I know. I really wanted to be a refrigerator.'''
+login_error = '''I'm sorry. I can't find that combination of credentials in my database. Perhaps you mis-typed your password?'''
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = True #TODO: put this in __main__?
 app.config['SECRET_KEY'] = SECRET_KEY
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 def logged_in(f):
     @wraps(f)
@@ -39,7 +50,7 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     next = request.args.get('next')
@@ -47,45 +58,171 @@ def login():
     if request.method == 'POST':
         uname = request.form['username']
         upswd = request.form['password']
-        authstr = uname + upswd
-        authhash = md5(authstr).hexdigest()
-        if authhash not in AUTH_USERS:
-            error = silly_text
+        upw = sha1(upswd).hexdigest()
+        
+        db = MySQLdb.connect(**CONNECTION_ARGS)
+        cur = db.cursor()
+        rows = cur.execute("select uname, pw, role from users where uname = %s",(uname,))
+        
+        if rows > 0:
+            users_name, pw_hash, role = cur.fetchone()
+        if rows == 0 or uname != users_name or upw != pw_hash:
+            error = login_error
         else:
             session['logged_in'] = True
             session['username'] = uname
-            session['role'] = AUTH_USERS[authhash]['role']
+            session['role'] = role
             
             if session['role'] == 'admin':
-                return redirect(url_for('dispacher'))
+                return redirect(url_for('dispatcher'))
             elif session['role'] == 'customer':
-                return redirect(url_for('tempTest'))
+                return redirect(url_for('repairRequest'))
+        cur.close()
+    return render_template('login.html', error=error, session=session, reqenviron = request.environ,)
 
-    return render_template('login.html', error=error, session=session)
-
-@app.route('/dispacher')
+@app.route('/resetdb')
+def resetdb():
+    host, port = request.environ['HTTP_HOST'].split(':')
+    db = MySQLdb.connect(**CONNECTION_ARGS)
+    cur = db.cursor()
+    rows = cur.execute("delete from jobs where j_ip = %s and j_port = %s", (host,port))
+    db.commit()
+    cur.close()
+    return jsonify({'host': host, 'port': port, 'rows_deleted': rows})
+    
+    
+@app.route('/dispatcher', methods=['GET', 'POST'])
 @logged_in
-def dispacher():
-    if session.get('role') == 'customer':
+def dispatcher():
+    error = None
+    db = MySQLdb.connect(**CONNECTION_ARGS)
+    cur = db.cursor()
+    if not session.get('logged_in'):
+        abort(401)
+    if session.get('role') != 'admin':
+        flash( 'please log in first' )
         return logout()
+    ##POST
+    if request.method == 'POST':
+        jid = request.form['job_id']
+        field = request.form['field_value']
+        new_value = request.form['new_value']
+
+        if field == 'job_status':
+            try:
+                affected_count = cur.execute("""update jobs set job_status = %s where j_id=%s""", (new_value, jid))
+                db.commit()
+                logger.info("%d", affected_count)
+                if affected_count > 0: flash( 'your update was successful' )
+            except MySQLdb.IntegrityError:
+                logger.warn("failed to update %s" % (jid,))
+                error = "failed to update job ID: %s" % (jid,)
+            finally:
+                cur.close()
+                
+        else:
+            if new_value.lower() == "null": new_value = None
+            try:
+                affected_count = cur.execute("""update jobs set appointment = %s where j_id=%s""", (new_value, jid))
+                db.commit()
+                logger.info("%d, %s", affected_count, new_value)
+                if affected_count > 0:
+                    flash( 'your update was successful' )
+            except MySQLdb.IntegrityError:
+                logger.warn("failed to update %s" % (jid,))
+                error = "failed to update job ID: %s" % (jid,)
+            finally:
+                cur.close()
+        
+        return redirect(url_for('dispatcher'))
+    
+    ##GET
     else:
-        db = MySQLdb.connect(**CONNECTION_ARGS)
-        cur = db.cursor()
-        cur.execute("select j.jid, c.cid, c.lname, j.make, j.appliance, j.job_status, j.appointment, j.description from jobs j, customer c where j.cid = c.cid")
+        cur.execute("select j.j_id, c.cid, c.lname, j.make, j.appliance, j.job_status, j.appointment, j.description from jobs j, customer c where j.cid = c.cid")
         result = cur.fetchall()
 
-
-        return render_template("dispacher.html",
-            title = "Dispacher Interface",
+        return render_template("dispatcher.html",
+            error = error,
+            title = "Dispatcher Interface",
             user = session['username'],
             reqenviron = request.environ,
             ses = session,
             result = result)
 
+@app.route('/repairRequest', methods=['GET', 'POST'])
+@logged_in
+def repairRequest():
+    session.permanent = False
+    error = None
+    user = session.get('username')
+    
+    db = MySQLdb.connect(**CONNECTION_ARGS)
+    cur = db.cursor()
+    if not session.get('logged_in'):
+        abort(401)
+    if session.get('role') != 'customer':
+        flash( 'please log in first' )
+        return logout()
+    host, port = request.environ['HTTP_HOST'].split(':')
+    
+    rows = cur.execute("select c.fname, c.lname, u.cid from customer c, users u where u.uname = %s and u.cid = c.cid",(user,))
+    if rows > 0:
+        fname, lname, cid = cur.fetchone()
+    else:
+        return logout()
+
+    #POST
+    if request.method == 'POST':
+        make = request.form['make']
+        type = request.form['type']
+        description = request.form['description']
+        appointment = None
+        job_status = 'pending'
+        logger.info("fname: %s, uname: %s, cid: %s", fname,lname,cid)
+        
+        rows = cur.execute("insert into jobs (j_ip,j_port,cid, make, appliance, appointment, job_status, description) values\
+            (%s, %s, %s, %s, %s, %s, %s, %s)", (host, port, cid, make, type, appointment, job_status, description))
+        
+        db.commit()
+        if rows == 0:
+            error = "your repair request failed"
+        
+        rows = cur.execute("select j_id, make, appliance, job_status, description from jobs where cid = %s and j_ip = %s and j_port = %s", (cid, host, port))
+        result = cur.fetchall()
+        
+        
+        
+        cur.close()        
+        return render_template("repairRequest.html",
+            title = "Repair Request",
+            user = user,
+            fname = fname,
+            lname = lname,
+            result = result,
+            error = error,
+            reqenviron = request.environ,
+            ses = session)
+    
+    
+    else:
+        cur.execute("select c.fname, c.lname from customer c, users u where u.uname = %s and u.cid = c.cid", (user,))
+        fname, lname = cur.fetchone()
+        return render_template("repairRequest.html",
+            title = "Repair Request",
+            user = user,
+            fname = fname,
+            lname = lname,
+            error = error,
+            reqenviron = request.environ,
+            ses = session)
+        cur.close()
+    
+
+           
+    
 @app.route('/testTemplate')
 @logged_in
 def tempTest():
-    session['objects'] = 42
     session.permanent = False
     
     return render_template("testTemplate.html",
@@ -109,11 +246,3 @@ def shutdown():
 
 if __name__ == "__main__":
     app.run(port=9229)
-    
-### RANDOM JUNK TO GET RID OF ###
-
-#                 if next:
-#                     flash('redirected ' + uname + ' to next')
-#                     return redirect(next)
-#                 else: return redirect(url_for('tempTest'))
-
--- make_AARdb.sql
+++ make_AARdb.sql
@@ -3,36 +3,120 @@ CREATE DATABASE AARdb
 
 USE AARdb;
 
+
 CREATE TABLE `customer` (
     `cid` mediumint NOT NULL auto_increment,
-    `fname` varchar(20) DEFAULT NULL,
+    `fname` varchar(20),
     `lname` varchar(20) NOT NULL,
+    `address` varchar(30),
+    `city` varchar(30),
+    `state` char(2),
+    `zip` mediumint,
+    `phone` char(12),
     primary key(cid)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `jobs` (
-  `jid` mediumint NOT NULL auto_increment,
-  `cid` mediumint,
-  `make` varchar(20) DEFAULT NULL,
-  `appliance` char(20) DEFAULT NULL,
-  `appointment` date DEFAULT NULL,
-  `job_status` enum('pending','approved','completed') DEFAULT NULL,
-  `description` varchar(140) default null,
-  primary key(jid),
-  foreign key(cid) references customer(cid)
+
+CREATE TABLE `users` (
+    `uname` varchar(8) NOT NULL,
+    `cid` mediumint,
+    `role` varchar(20) NOT NULL,
+    `pw` varchar(40) NOT NULL,
+    primary key(uname),
+    foreign key(cid) references customer(cid)
 ) ENGINE=InnoDB;
 
-insert into customer (fname,lname) values
-    ('Miguel','Cervantes'),
-    ('Eleanor','Roosevelt');
+CREATE TABLE `jobs` (
+    `j_ip` varchar(20),
+    `j_port` mediumint,
+    `j_id` mediumint NOT NULL AUTO_INCREMENT,
+    `cid` mediumint,
+    `make` varchar(30),
+    `appliance` char(30),
+    `appointment` date,
+    `job_status` enum('pending','completed'),
+    `description` varchar(140),
+    PRIMARY KEY (j_id),
+    foreign key(cid) references customer(cid)
+) ENGINE=InnoDB;
 
-insert into jobs (cid, make, appliance, appointment, job_status, description) values
-    (1,'maytag','washer', null, 'pending', "outflow hoses leak"),
-    (2,'GE','refrigerator', '2013-11-05', 'approved', "Ices up; won't defrost");
-    
+ALTER TABLE jobs auto_increment=100;
+
+insert into customer (fname,lname,address,city,state,zip,phone) values
+    ('Mary', 'Brown', '4531 Hawthorn', 'Centerville', 'PA', '28634', '234-555-4399'),
+    ('John', 'Smith', '6296 Plum', 'Arlington', 'AR', '22041', '809-555-7189'),
+    ('Robert', 'Lambright', '1286 Willow', 'Salem', 'AR', '24226', '758-555-6579'),
+    ('Chandra', 'Asam', '325 Spruce', 'Manchester', 'SD', '12204', '303-555-5006'),
+    ('James', 'McNiel', '3575 Walnut', 'Jackson', 'NY', '66044', '412-555-2351'),
+    ('Spencer', 'Nasem', '1826 Birch', 'Lexington', 'NJ', '04942', '671-555-6544'),
+    ('Christian', 'Serda', '2122 Birch', 'Dover', 'NM', '30434', '858-555-5659'),
+    ('Joseph', 'Crosley', '5081 Tulip', 'Greenville', 'NE', '96034', '516-555-2904'),
+    ('Sandra', 'Edridge', '8917 Lilac', 'Franklin', 'RI', '17062', '972-555-9010'),
+    ('Felipa', 'Elsey', '1685 Pawpaw', 'Greenville', 'LA', '04942', '813-555-4748'),
+    ('Marsha', 'Mulhollen', '9335 Hazel', 'Newport', 'AZ', '89803', '614-555-6941'),
+    ('Leslie', 'Balitas', '7638 Buckeye', 'Lexington', 'ND', '37079', '872-555-2202'),
+    ('Mark', 'Gustison', '5383 Redbud', 'Auburn', 'NJ', '16375', '919-555-5086'),
+    ('Teresa', 'Deutsch', '6362 Birch', 'Georgetown', 'MI', '96034', '709-555-6338'),
+    ('Katelyn', 'Gerst', '3418 Aspen', 'Centerville', 'SD', '02163', '938-555-9142'),
+    ('Wanda', 'Varnell', '9674 Fir', 'Burlington', 'ME', '28634', '573-555-2084'),
+    ('Bree', 'Bjorklund', '5670 Catalpa', 'Jackson', 'NM', '24076', '947-555-5635'),
+    ('Hank', 'Imming', '824 Catalpa', 'Jackson', 'VT', '66720', '682-555-8637'),
+    ('Siu', 'Chong', '4916 Buckeye', 'Clinton', 'RI', '02904', '204-555-7901'),
+    ('Michelle', 'Schartz', '2152 Cottonwood', 'Milford', 'TX', '98166', '268-555-6167'),
+    ('Karie', 'Elldrege', '8450 Aspen', 'Marion', 'NC', '05152', '541-555-8377'),
+    ('Jeffry', 'Bolte', '1083 Crabapple', 'Centerville', 'KS', '30434', '815-555-5037'),
+    ('Dalia', 'Castle', '9560 Palm', 'Georgetown', 'IA', '02904', '816-555-9330'),
+    ('Angel', 'Rodriquez', '4439 Tupelo', 'Dayton', 'KY', '22041', '970-555-9433'),
+    ('Eleanor', 'Stevick', '1601 Birch', 'Auburn', 'OH', '85942', '289-555-8844'),
+    ('Rita', 'Zulkowski', '2995 Alder', 'Dayton', 'PA', '22907', '850-555-6438'),
+    ('Brian', 'Borsos', '1728 Filbert', 'Centerville', 'WA', '43119', '217-555-1776'),
+    ('Hayden', 'Tourtellotte', '7306 Tupelo', 'Jackson', 'MN', '16375', '762-555-8919'),
+    ('Astrid', 'Tardio', '225 Tamarack', 'Auburn', 'MT', '43119', '971-555-9385'),
+    ('Agatha', 'Trench', '9593 Holly', 'Franklin', 'ND', '15642', '708-555-4531');
+
+insert into jobs (j_ip,j_port,cid, make, appliance, appointment, job_status, description) values
+    ('localhost',9229,1,'maytag','washer', null, 'pending', "outflow hoses leak"),
+    ('localhost',9229,2,'GE','refrigerator', '2013-11-05', 'completed', "Ices up; won't defrost");
+
 # notes
+
+# for multiple column primary key, can't use autoincrement. Job Id's will be generated in the code.
+
 # ALTER TABLE Orders
 # ADD FOREIGN KEY (P_Id)
 # REFERENCES Persons(P_Id)
 
 # ALTER TABLE jobs add column tid mediumint, add foreign key(tid) references tech(tid);
+# SELECT j.jid, c.cid, c.lname, j.make, j.appliance, j.job_status, j.appointment, j.description
+# FROM jobs j, customer c
+# WHERE j.cid = c.cid;
+
+insert into jobs (j_ip,j_port,cid, make, appliance, appointment, job_status, description) values
+    ('000.000.000.000',8080,3,'Maytag','Washer', null, 'pending', "outflow hoses leak"),
+    ('000.000.000.000',8080,4,'GE','Refrigerator', '2013-11-01', 'completed', "Ices up; won't defrost"),
+    ('000.000.000.000',8080,5,'Alessi','Teapot', null, 'pending', "explodes"),
+    ('000.000.000.000',8080,6,'Amana','Range', '2013-11-02', 'completed', "oven heats unevenly"),
+    ('000.000.000.000',8080,7,'Whirlpool','Refrigerator', '2013-11-03', 'pending', "Makes a rattling noise"),
+    ('000.000.000.000',8080,8,'GE','Microwave', '2013-11-04', 'pending', "Sparks and smokes when I put forks in it"),
+    ('000.000.000.000',8080,9,'Maytag','Drier', null, 'pending', "Never heats up"),
+    ('000.000.000.000',8080,10,'Amana','Refrigerator', '2013-11-05', 'pending', "Temperature too low, can't adjust."),
+    ('000.000.000.000',8080,11,'Samsung','Washer', null, 'pending', "Doesn't get my bear suit white"),
+    ('000.000.000.000',8080,12,'Frigidaire','Refrigerator', '2013-11-06', 'completed', "Has a bad smell I can't get rid of."),
+    ('000.000.000.000',8080,13,'In-Sink-Erator','Dispose-all', null, 'pending', "blades broken"),
+    ('000.000.000.000',8080,14,'KitchenAid','Mixer', '2013-11-07', 'completed', "Blows my fuses"),
+    ('000.000.000.000',8080,15,'Moulinex','Juicer', null, 'pending', "Won't start"),
+    ('000.000.000.000',8080,16,'Viking','Range', '2013-11-08', 'completed', "Gas leak"),
+    ('000.000.000.000',8080,17,'Aga','Range', null, 'pending', "burner cover is cracked"),
+    ('000.000.000.000',8080,18,'Jennaire','Cooktop', '2013-11-09', 'completed', "Glass cracked"),
+    ('000.000.000.000',8080,19,'Wolfe','Stove', null, 'pending', "Burners are uneven"),
+    ('000.000.000.000',8080,20,'LG','Dehumidifier', '2013-11-10', 'pending', "Ices up when external temp is around freezing"),
+    ('000.000.000.000',8080,21,'De’Longhi','Oil Space Heater', null, 'pending', "Smells bad"),
+    ('000.000.000.000',8080,22,'Kenmore','Refrigerator', '2013-11-11', 'pending', "excessive vibration"),
+    ('000.000.000.000',8080,23,'Maytag','Washer/Drier', null, 'pending', "outflow hoses leak"),
+    ('000.000.000.000',8080,24,'GE','Refrigerator', '2013-11-12', 'pending', "Refrigerator light is defective"),
+    ('000.000.000.000',8080,25,'Kenmore','Washer', null, 'pending', "Unbalanced spin cycle"),
+    ('000.000.000.000',8080,26,'Cookmore','Outdoor Grill', '2013-11-13', 'pending', "Smoker box is stuck"),
+    ('000.000.000.000',8080,27,'Kenmore','Water heater', null, 'pending', "Can't adjust temperature"),
+    ('000.000.000.000',8080,28,'Sanyo','Minifridge', '2013-11-14', 'pending', "Makes a lot of noise"),
+    ('000.000.000.000',8080,29,'Bosch','Dishwasher', null, 'pending', "leaves spots on my glases"),
+    ('000.000.000.000',8080,30,'Whirlpool','Trash Compactor', '2013-11-15', 'pending', "leaking hydraulic fluid");
